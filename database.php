40c9e20c191c0eaca27d440dc53e4935e123b7da
unknown <diaz.270303@gmail.com>
2025-06-29 19:49:36 +0700

    menambahkan file database php di api-config

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $database_name = "wisata_ciamis";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->database_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
