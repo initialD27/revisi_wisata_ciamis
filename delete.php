4d4cbe0cf4c6c209447100d3d57e846758c4792b
initialD27 <diaz.270303@gmail.com>
2025-06-29 21:17:45 +0700

    menambahkan file delete php di api-gallery

--- delete.php
+++ delete.php
@@ -1,24 +1,24 @@
 <?php
 require_once '../config/headers.php';
 require_once '../config/database.php';
-require_once '../objects/destination.php';
+require_once '../objects/gallery.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$destination = new Destination($db);
+$gallery = new Gallery($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
 if(!empty($data->id)) {
-    $destination->id = $data->id;
-
-    if($destination->delete()) {
+    $gallery->id = $data->id;
+    
+    if($gallery->delete()) {
         http_response_code(200);
-        echo json_encode(array("message" => "Destinasi wisata berhasil dihapus."));
+        echo json_encode(array("message" => "Foto berhasil dihapus."));
     } else {
         http_response_code(503);
-        echo json_encode(array("message" => "Tidak dapat menghapus destinasi wisata."));
+        echo json_encode(array("message" => "Tidak dapat menghapus foto."));
     }
 } else {
     http_response_code(400);
