1e3d27819757232ea698f17601338278174f13a4
initialD27 <diaz.270303@gmail.com>
2025-06-29 21:09:08 +0700

    menambahkan file create php di api-gallery

--- create.php
+++ create.php
@@ -1,40 +1,32 @@
 <?php
 require_once '../config/headers.php';
 require_once '../config/database.php';
-require_once '../objects/destination.php';
+require_once '../objects/gallery.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$destination = new Destination($db);
+$gallery = new Gallery($db);
 
-if(
-    !empty($_POST['name']) &&
-    !empty($_POST['description']) &&
-    !empty($_POST['location'])
-) {
-    $destination->name = $_POST['name'];
-    $destination->description = $_POST['description'];
-    $destination->location = $_POST['location'];
+if(isset($_FILES['image'])) {
+    $target_dir = "../../assets/images/gallery/";
+    $file_extension = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+    $file_name = uniqid() . '.' . $file_extension;
+    $target_file = $target_dir . $file_name;
     
-    // Handle image upload
-    if(isset($_FILES['image'])) {
-        $target_dir = "../../assets/images/best-destination/";
-        $file_extension = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
-        $file_name = uniqid() . '.' . $file_extension;
-        $target_file = $target_dir . $file_name;
+    if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        $gallery->image_url = "assets/images/gallery/" . $file_name;
         
-        if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-            $destination->image_url = "assets/images/best-destination/" . $file_name;
+        if($gallery->create()) {
+            http_response_code(201);
+            echo json_encode(array("message" => "Foto berhasil ditambahkan."));
+        } else {
+            http_response_code(503);
+            echo json_encode(array("message" => "Tidak dapat menambahkan foto."));
         }
-    }
-
-    if($destination->create()) {
-        http_response_code(201);
-        echo json_encode(array("message" => "Destinasi wisata berhasil dibuat."));
     } else {
         http_response_code(503);
-        echo json_encode(array("message" => "Tidak dapat membuat destinasi wisata."));
+        echo json_encode(array("message" => "Gagal mengupload foto."));
     }
 } else {
     http_response_code(400);
