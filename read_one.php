b242c0a10e65e2079eb13f89b0536c0ad494c34f
unknown <diaz.270303@gmail.com>
2025-06-29 20:01:38 +0700

    menambahkan file read_one php di api-destination

--- read_one.php
+++ read_one.php
@@ -1,31 +1,28 @@
 <?php
 require_once '../config/headers.php';
 require_once '../config/database.php';
-require_once '../objects/about.php';
+require_once '../objects/destination.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$about = new About($db);
+$destination = new Destination($db);
+$destination->id = isset($_GET['id']) ? $_GET['id'] : die();
 
-if(isset($_GET['id'])) {
-    $about->id = $_GET['id'];
-    $about->readOne();
+if($destination->readOne()) {
+    $destination_arr = array(
+        "id" => $destination->id,
+        "name" => $destination->name,
+        "description" => $destination->description,
+        "location" => $destination->location,
+        "image_url" => $destination->image_url,
+        "created_at" => $destination->created_at,
+        "updated_at" => $destination->updated_at
+    );
 
-    if($about->nama_kota != null) {
-        $about_arr = array(
-            "id" => $about->id,
-            "nama_kota" => $about->nama_kota,
-            "deskripsi" => $about->deskripsi,
-            "foto" => $about->foto
-        );
-        http_response_code(200);
-        echo json_encode($about_arr);
-    } else {
-        http_response_code(404);
-        echo json_encode(array("message" => "Data tidak ditemukan."));
-    }
+    http_response_code(200);
+    echo json_encode($destination_arr);
 } else {
-    http_response_code(400);
-    echo json_encode(array("message" => "ID tidak ditemukan."));
+    http_response_code(404);
+    echo json_encode(array("message" => "Destinasi tidak ditemukan."));
 }
