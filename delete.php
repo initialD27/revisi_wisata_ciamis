7bf81f790b43baaca6bb6d1a8138c7f1d59450c1
unknown <diaz.270303@gmail.com>
2025-06-29 20:00:52 +0700

    menambahkan file delete php di api-destination

--- delete.php
+++ delete.php
@@ -1,24 +1,24 @@
 <?php
 require_once '../config/headers.php';
 require_once '../config/database.php';
-require_once '../objects/about.php';
+require_once '../objects/destination.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$about = new About($db);
+$destination = new Destination($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
 if(!empty($data->id)) {
-    $about->id = $data->id;
-    
-    if($about->delete()) {
+    $destination->id = $data->id;
+
+    if($destination->delete()) {
         http_response_code(200);
-        echo json_encode(array("message" => "Data berhasil dihapus."));
+        echo json_encode(array("message" => "Destinasi wisata berhasil dihapus."));
     } else {
         http_response_code(503);
-        echo json_encode(array("message" => "Gagal menghapus data."));
+        echo json_encode(array("message" => "Tidak dapat menghapus destinasi wisata."));
     }
 } else {
     http_response_code(400);
